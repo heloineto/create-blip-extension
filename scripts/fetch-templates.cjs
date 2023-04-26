bb0b70709a30ef1f0b48fde9a88f57a5519a26bb
Heloi Neto <hey@heloineto.com>
2023-04-25 22:47:49 -0300

    feat: add refetch-templates script

---
+++ scripts/fetch-templates.cjs
@@ -0,0 +1,44 @@
+const { execSync } = require('node:child_process');
+const path = require('node:path');
+const fs = require('node:fs');
+const cwd = process.cwd();
+
+const fetchTemplate = (branch) => {
+    const templateName = `template-${branch}`;
+    const templatePath = path.join(cwd, templateName);
+
+    execSync(
+        `npx degit heloineto/blip-extension-template#${branch} ${templateName} --force`
+    );
+
+    fs.renameSync(
+        path.join(templatePath, '.gitignore'),
+        path.join(templatePath, '_gitignore')
+    );
+
+    fs.copyFileSync(
+        path.join(templatePath, '/src/config/appsettings.json'),
+        path.join(templatePath, '/src/config/appsettings.development.json')
+    );
+
+    // Delete the scripts folder
+    fs.rmdirSync(path.join(templatePath, '/scripts'), { recursive: true });
+
+    // get the package.json
+    const packageJson = require(path.join(templatePath, '/package.json'));
+    // Remove the postinstall script from package.json
+    delete packageJson.scripts.postinstall;
+
+    // Overwrite the package.json
+    fs.writeFileSync(
+        path.join(templatePath, '/package.json'),
+        JSON.stringify({ packageJson }, null, 4)
+    );
+};
+
+const fetchTemplates = () => {
+    fetchTemplate('typescript');
+    fetchTemplate('javascript');
+};
+
+fetchTemplates();
