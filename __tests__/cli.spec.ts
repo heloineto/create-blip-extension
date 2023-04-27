3d5c5f86d00cf43a7baa533e9e9a5fbdb1912fa8
Heloi Neto <hey@heloineto.com>
2023-04-26 23:19:24 -0300

    feat: add tests

---
+++ __tests__/cli.spec.ts
@@ -0,0 +1,93 @@
+import { join } from 'node:path';
+import type { ExecaSyncReturnValue, SyncOptions } from 'execa';
+import { execaCommandSync } from 'execa';
+import fs from 'fs-extra';
+import { afterEach, beforeAll, expect, test } from 'vitest';
+
+const CLI_PATH = join(__dirname, '..');
+
+const projectName = 'test-app';
+const genPath = join(__dirname, projectName);
+
+const run = (
+    args: string[],
+    options: SyncOptions = {}
+): ExecaSyncReturnValue => {
+    return execaCommandSync(`node ${CLI_PATH} ${args.join(' ')}`, options);
+};
+
+const createNonEmptyDir = () => {
+    fs.mkdirpSync(genPath);
+
+    const pkgJson = join(genPath, 'package.json');
+    fs.writeFileSync(pkgJson, '{ "foo": "bar" }');
+};
+
+const templateFiles = fs
+    .readdirSync(join(CLI_PATH, 'template-typescript'))
+    .map((filePath) => (filePath === '_gitignore' ? '.gitignore' : filePath))
+    .sort();
+
+beforeAll(() => fs.remove(genPath));
+afterEach(() => fs.remove(genPath));
+
+test('prompts for the project name if none supplied', () => {
+    const { stdout } = run([]);
+    expect(stdout).toContain('Project name:');
+});
+
+test('prompts for the template if none supplied when target dir is current directory', () => {
+    fs.mkdirpSync(genPath);
+    const { stdout } = run(['.'], { cwd: genPath });
+    expect(stdout).toContain('Select a template:');
+});
+
+test('prompts for the template if none supplied', () => {
+    const { stdout } = run([projectName]);
+    expect(stdout).toContain('Select a template:');
+});
+
+test('prompts for the template on not supplying a value for --template', () => {
+    const { stdout } = run([projectName, '--template']);
+    expect(stdout).toContain('Select a template:');
+});
+
+test('prompts for the template on supplying an invalid template', () => {
+    const { stdout } = run([projectName, '--template', 'unknown']);
+    expect(stdout).toContain(
+        `"unknown" isn't a valid template. Please choose from below:`
+    );
+});
+
+test('asks to overwrite non-empty target directory', () => {
+    createNonEmptyDir();
+    const { stdout } = run([projectName], { cwd: __dirname });
+    expect(stdout).toContain(`Target directory "${projectName}" is not empty.`);
+});
+
+test('asks to overwrite non-empty current directory', () => {
+    createNonEmptyDir();
+    const { stdout } = run(['.'], { cwd: genPath });
+    expect(stdout).toContain(`Current directory is not empty.`);
+});
+
+test('successfully scaffolds a project based on the starter templates', () => {
+    const { stdout } = run([projectName, '--template', 'typescript'], {
+        cwd: __dirname
+    });
+    const generatedFiles = fs.readdirSync(genPath).sort();
+
+    expect(stdout).toContain(`Scaffolding project in ${genPath}`);
+    expect(templateFiles).toEqual(generatedFiles);
+});
+
+test('works with the -t alias', () => {
+    const { stdout } = run([projectName, '-t', 'typescript'], {
+        cwd: __dirname
+    });
+    const generatedFiles = fs.readdirSync(genPath).sort();
+
+    // Assertions
+    expect(stdout).toContain(`Scaffolding project in ${genPath}`);
+    expect(templateFiles).toEqual(generatedFiles);
+});
